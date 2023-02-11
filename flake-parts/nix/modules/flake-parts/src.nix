a62fbf225ec6e94f1b5f22a33d0e358b68fde728
DavHau <hsngrmpf+github@gmail.com>
2023-02-11 17:01:48 +0700

    add src.nix

---
+++ flake-parts/nix/modules/flake-parts/src.nix
@@ -0,0 +1,15 @@
+{ self, lib, inputs, ... }: let
+  nix-filter = inputs.nix-filter.lib;
+in {
+  # options.src = lib.mkOption { type = lib.types.raw; };
+  flake.src = nix-filter {
+    root = "${self}";
+    # include = [];
+    exclude = [
+      # exclude the ./nix directory
+      "nix"
+      # exclude all .nix files at all locations
+      (nix-filter.matchExt ".nix")
+    ];
+  };
+}
